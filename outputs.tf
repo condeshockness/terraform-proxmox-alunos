6627d59b9943eaded9abe0fbba40c492ddd5ee87
condeshockness <ramonsilas.dev@gmail.com>
2026-01-09 12:03:59 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,14 @@
 output "alunos_lxc_ids" {
   value = {
     for k, m in module.alunos_lxc :
-    k => m.lxc_id
+    k => m.id
+  }
+}
+
+output "alunos_lxc_vmids" {
+  value = {
+    for k, m in module.alunos_lxc :
+    k => m.vmid
   }
 }
 
